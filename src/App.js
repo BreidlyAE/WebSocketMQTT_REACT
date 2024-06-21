deb0345e137cafcbf714767bb44ec383736d7816
Breidly <u23228648@utp.edu.pe>
2024-06-21 14:45:16 -0500

    React - Comunicacion en 2 Topicos

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { io } from "socket.io-client";
@@ -10,14 +11,33 @@ socket.on("maestro1/puerto2", (data) => {
 });
 //=====================================================================
 function App() {
+  const [timestamp, setTimestamp] = useState(0);
+
+  const handleMouseDown = (event) => {
+    console.log("Boton_presionado:", true);
+    socket.emit("boton_presionadoSocketIO", true);
+    setTimestamp(Date.now());
+  };
+
+  const handleMouseUp = (event) => {
+    console.log("boton_presionado:", false);
+    socket.emit("boton_presionadoSocketIO", false);
+    console.log(Date.now() - timestamp);
+  };
+
   return (
     <div className="App">
       <button
-        onClick={() => {
+        onClick={(event) => {
+          const message = ":v" + Date.now();
           // ENVIO DE DATOS ==================================================
-          socket.emit("from_react", ":v " + Date.now());
+          socket.emit("from_react", message);
           //==================================================================
+          console.log("Mensaje enviado:", message);
+          console.log("Evento de clic", event);
         }}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
       >
         Enviar
       </button>
